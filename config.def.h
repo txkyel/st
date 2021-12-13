017a7910a6c3f26ee7d86c3b97fb48e5a0ec1737
Kyle <kylexiao20@gmail.com>
2021-12-13 00:50:46 +0000

    Rework st Xresources colours

--- config.def.h
+++ config.def.h
@@ -102,32 +102,30 @@ float alpha = 0.8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	[0] = "black",
+	[1] = "red3",
+	[2] = "green3",
+	[3] = "yellow3",
+	[4] = "blue2",
+	[5] = "magenta3",
+	[6] = "cyan3",
+	[7] = "gray90",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	[8]  = "gray50",
+	[9]  = "red",
+	[10] = "green",
+	[11] = "yellow",
+	[12] = "#5c5cff",
+	[13] = "magenta",
+	[14] = "cyan",
+	[15] = "white",
 
-	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"grey90",
-	"black",
+	[256] = "#cccccc",
+	[257] = "#555555",
+	[258] = "white",
 };
 
 
@@ -135,10 +133,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultfg = 257;
+static unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 258;
 
 /*
  * Default shape of cursor
@@ -198,9 +196,9 @@ ResourcePref resources[] = {
 		{ "color13",      STRING,  &colorname[13] },
 		{ "color14",      STRING,  &colorname[14] },
 		{ "color15",      STRING,  &colorname[15] },
-		{ "cursorColor",  STRING,  &colorname[256] },
-		{ "foreground",   STRING,  &colorname[258] },
-		{ "background",   STRING,  &colorname[259] },
+		{ "background",   STRING,  &colorname[256] },
+		{ "foreground",   STRING,  &colorname[257] },
+		{ "color7",       STRING,  &colorname[258] },
 		{ "termname",     STRING,  &termname },
 		{ "shell",        STRING,  &shell },
 		{ "minlatency",   INTEGER, &minlatency },
